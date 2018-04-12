fb2da78637a420d9a022a4325672ef3e9c42f0bf
SkeletorMartin <37380486+SkeletorMartin@users.noreply.github.com>
2018-04-12 16:06:58 -0500

    Update roomChange.java

--- roomChange.java
+++ roomChange.java
@@ -5,5 +5,30 @@ package contemporyproject;
  * @author Colton
  */
 public class roomChange {
+    private int roomX = 0;
+    private int roomY = 0;
     
+    public void changeRoom(int xChange, int yChange){
+        if (roomX > 0 && roomX < 11 && roomY > 0 && roomY < 7){
+            roomX += xChange;
+            roomY += yChange;
+        }
+        return;
+    }
+    
+    public int getRoomX(){
+        return roomX;
+    }
+    
+    public int getRoomY(){
+        return roomY;
+    }
+    
+    public void setRoomX(int x){
+        roomX = x;
+    }
+    
+    public void setRoomY(int y){
+        roomY = y;
+    } 
 }
