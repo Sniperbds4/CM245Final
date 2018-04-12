3bc26a74e3ab7d66df9f80b7aac130a4c197fb30
SkeletorMartin <37380486+SkeletorMartin@users.noreply.github.com>
2018-04-12 17:50:28 -0500

    Update rEncounter.java

--- rEncounter.java
+++ rEncounter.java
@@ -4,5 +4,21 @@ package finalProject;
  * Random Encounter Creator
  * @author Colton
  */
+import java.util.Random;
+
 public class rEncounter{
-    private 
+    private int encStr = 1;
+    private int index = 0;
+    
+    public boolean ranEncounter(){
+        Random rand = new Random;
+        index = rand.nextInt(10) + 1;
+        
+        if (index >= encStr)
+            encStr++;
+            return;
+        else{
+            //create enemy
+        }
+    }
+ }
