fe3a8d8fd50a8c3313293ebc3f0a784b67362a6f
SkeletorMartin <37380486+SkeletorMartin@users.noreply.github.com>
2018-04-12 18:00:44 -0500

    Add files via upload

---
+++ Char.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Nicholas Feuerborn
+ */
+public class Char {
+        int currentHealth = 20;
+        int maxHealth = 20;
+        int critical = 10;
+        int missRate = 1;
+        
+        public int getCurrentHealth(){
+            return this.currentHealth;
+        }
+        public int setCurrentHealth(int value){
+            return this.currentHealth = value;
+        }
+        
+        public int getMaxHealth(){
+            return this.maxHealth;
+        }
+        public int setMaxHealth(int value){
+            return this.maxHealth = value;
+        }
+        
+        public int getCritical(){
+            return this.critical;
+        }
+        public int setCritical(int value){
+            return this.currentHealth = value;
+        } 
+        
+        public int getMissRate(){
+            return this.missRate;
+        }
+        public int setMissRate(int value){
+            return this.missRate = value;
+        }
+    }
